18713f76ecdfab3b44a77b8112338947c21dfc86
SOMESH KUMAR DHRUW <140878452+Somesh-kumar-dhruw@users.noreply.github.com>
2023-12-20 14:41:03 +0530

    Update mongodb url link in this project

--- Backend/config-file/config.js
+++ Backend/config-file/config.js
@@ -1,7 +1,7 @@
 // Exported configuration object with MongoDB and JWT secret values
 module.exports = {
     // MongoDB connection URL
-    MONGODB_URL: "mongodb+srv://ajaykumar:<Rohit963kumar>@cluster0.ogbgrwv.mongodb.net/?retryWrites=true&w=majority",
+    MONGODB_URL: "mongodb+srv://ajaykumar:Rohit963Kumar@cluster0.ogbgrwv.mongodb.net/?retryWrites=true&w=majority",
     
     // JSON Web Token (JWT) secret key
     JWT_SECRET: 'hkdsooewioi3849329929023kdjflado'
